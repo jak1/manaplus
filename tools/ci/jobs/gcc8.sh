0385681a68c8be8dccc9f592d83fa2017652694a
jak1 <mike.wollmann@gmail.com>
2020-05-23 12:29:26 +0200

    changed to portable version

--- tools/ci/jobs/gcc8.sh
+++ tools/ci/jobs/gcc8.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+dir=`pwd`
 
 export CC=gcc-8
 export CXX=g++-8
@@ -24,8 +25,16 @@ source ./tools/ci/flags/gcc8.sh
 export CXXFLAGS="$CXXFLAGS $POST_CXXFLAGS"
 
 do_init
-run_configure --enable-werror $*
+run_configure --enable-werror \
+--enable-commandlinepassword \
+--datadir=$dir/run/share/games \
+--bindir=$dir/run/bin \
+--mandir=$dir/run/share/man \
+--enable-portable=yes $*
+
 run_make
+run_make_install
+
 
 source ./tools/ci/scripts/exit.sh
 
